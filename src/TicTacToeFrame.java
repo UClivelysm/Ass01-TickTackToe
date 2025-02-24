38970c49480ad15146e541e42867996f15977195
UClivelysm <livelysm@mail.uc.edu>
2025-02-24 17:50:16 -0500

    Finished

---
+++ src/TicTacToeFrame.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TicTacToeFrame extends JFrame {
+    private TicTacToeTile[][] board;
+    private String currentPlayer = "X";
+
+
+
+
+    public TicTacToeFrame() {
+        board = new TicTacToeTile[3][3];
+        setLayout(new GridLayout(3, 3));
+        CreateBoard();
+        setTitle("Tic-Tac-Toe");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    private void CreateBoard() {
+        TicTacToeActionListener listener = new TicTacToeActionListener(this);
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = new TicTacToeTile(row, col);
+                board[row][col].setText(" ");
+                board[row][col].addActionListener(listener);
+                add(board[row][col]);
+            }
+        }
+    }
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void switchPlayer() {
+        if (currentPlayer.equals("X")) {
+            currentPlayer = "O";
+        } else {
+            currentPlayer = "X";
+        }
+    }
+    public TicTacToeTile[][] getBoard() {
+        return board;
+    }
+
+    public boolean isWin(String player) {
+        return isRowWin(player) || isColWin(player) || isDiagonalWin(player);
+    }
+
+    private boolean isRowWin(String player) {
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].getText().equals(player) &&
+                    board[row][1].getText().equals(player) &&
+                    board[row][2].getText().equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isColWin(String player) {
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].getText().equals(player) &&
+                    board[1][col].getText().equals(player) &&
+                    board[2][col].getText().equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDiagonalWin(String player) {
+        return (board[0][0].getText().equals(player) &&
+                board[1][1].getText().equals(player) &&
+                board[2][2].getText().equals(player)) ||
+                (board[0][2].getText().equals(player) &&
+                        board[1][1].getText().equals(player) &&
+                        board[2][0].getText().equals(player));
+    }
+
+    public boolean isTie() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col].getText().equals(" ")) {
+                    return false; // Empty space found, not a tie
+                }
+            }
+        }
+        return !isWin("X") && !isWin("O");
+    }
+    public void resetBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col].setText(" ");
+            }
+        }
+        this.currentPlayer = "X";
+    }
+
+
+
+}
+
+
+
